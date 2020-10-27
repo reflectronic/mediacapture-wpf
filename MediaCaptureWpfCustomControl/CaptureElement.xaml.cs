eaa9ec77ae68da81689cd29ca128d968d8830fe0
reflectronic <john-tur@outlook.com>
2020-10-26 20:59:16 -0400

    Add project files.

---
+++ MediaCaptureWpfCustomControl/CaptureElement.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Storage.Streams;
+
+namespace MediaCaptureWpfCustomControl
+{
+    /// <summary>
+    /// Interaction logic for CaptureElement.xaml
+    /// </summary>
+    public partial class CaptureElement : UserControl
+    {
+        WriteableBitmap writeableBitmap;
+        SoftwareBitmap backBuffer;
+
+        bool taskRunning;
+
+        public CaptureElement()
+        {
+            InitializeComponent();
+        }
+
+        public async Task SetCaptureAsync(MediaCapture capture)
+        {
+            await capture.InitializeAsync(new() 
+            {
+                MemoryPreference = MediaCaptureMemoryPreference.Cpu
+
+            });
+
+            // TODO: Likely should use APIs which can guarantee us the proper media sources and formats
+            // https://docs.microsoft.com/en-us/windows/uwp/audio-video-camera/process-media-frames-with-mediaframereader#select-frame-sources-and-frame-source-groups
+            var video = capture.FrameSources.First().Value;
+            var reader = await capture.CreateFrameReaderAsync(video);
+
+            writeableBitmap = new(
+                (int) video.CurrentFormat.VideoFormat.Width, 
+                (int) video.CurrentFormat.VideoFormat.Height, 
+                96,
+                96,
+                PixelFormats.Bgra32, 
+                null);
+
+            PreviewImage.Source = writeableBitmap;
+
+            reader.FrameArrived += Reader_FrameArrived;
+            var thing = await reader.StartAsync();
+        }
+
+        private void Reader_FrameArrived(Windows.Media.Capture.Frames.MediaFrameReader sender, Windows.Media.Capture.Frames.MediaFrameArrivedEventArgs args)
+        {
+            using var mediaFrameReference = sender.TryAcquireLatestFrame();
+            var videoMediaFrame = mediaFrameReference?.VideoMediaFrame;
+            var softwareBitmap = videoMediaFrame?.SoftwareBitmap;
+
+            if (softwareBitmap != null)
+            {
+                if (softwareBitmap is { BitmapPixelFormat: not BitmapPixelFormat.Bgra8 } or { BitmapAlphaMode: not BitmapAlphaMode.Premultiplied })
+                {
+                    softwareBitmap = SoftwareBitmap.Convert(softwareBitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+                }
+
+                softwareBitmap = Interlocked.Exchange(ref backBuffer, softwareBitmap);
+                softwareBitmap?.Dispose();
+
+                writeableBitmap.Dispatcher.Invoke(() =>
+                {
+                    if (taskRunning)
+                    {
+                        return;
+                    }
+
+                    taskRunning = true;
+
+                    SoftwareBitmap latestBitmap;
+                    while ((latestBitmap = Interlocked.Exchange(ref backBuffer, null)) != null)
+                    {
+                        try
+                        {
+                            writeableBitmap.Lock();
+
+                            using var m = latestBitmap.LockBuffer(BitmapBufferAccessMode.Read);
+                            using var reference = m.CreateReference();
+
+                            var t = m.GetPlaneDescription(0);
+
+                            unsafe
+                            {
+                                ((IMemoryBufferByteAccess) reference).GetBuffer(out var ptr, out var capacity);
+
+                                writeableBitmap.WritePixels(
+                                    new Int32Rect(0, 0, writeableBitmap.PixelWidth, writeableBitmap.PixelHeight), 
+                                    (IntPtr)ptr, 
+                                    (int)capacity, 
+                                    t.Stride);
+                            }
+
+                            latestBitmap.Dispose();
+
+                            writeableBitmap.AddDirtyRect(new Int32Rect(0, 0, writeableBitmap.PixelWidth, writeableBitmap.PixelHeight));
+
+                        }
+                        finally
+                        {
+                            writeableBitmap.Unlock();
+                        }
+
+                    }
+
+                    taskRunning = false;
+                });
+            }
+        }
+    }
+
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    unsafe interface IMemoryBufferByteAccess
+    {
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
+}
---
+++ MediaCaptureWpfCustomControl/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+using Windows.Media.Capture;
+
+namespace MediaCaptureWpfCustomControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaCapture mediaCaptureMgr = new MediaCapture();
+            MyCaptureElement.SetCaptureAsync(mediaCaptureMgr);
+           // await mediaCaptureMgr.StartPreviewAsync();
+        }
+    }
+}
